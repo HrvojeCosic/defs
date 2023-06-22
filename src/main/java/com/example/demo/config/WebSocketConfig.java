ede3ba53b4882971a7812d62ded3838fa8aee583
dominikandelic <dominik.andelic@em2.hr>
2023-06-22 22:18:56 +0200

    Add allowed origins to stomp endpoints

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -18,8 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS();
-
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*://localhost:*")
+                .withSockJS();
     }
 }
